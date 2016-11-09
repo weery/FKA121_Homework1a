41c043ec58472541206a29b5a8ba33b45968a4f3
Weery <vicnilss@student.chalmers.se>
2016-11-09 09:30:30 +0100

    Initial layout

--- code/MD_main.c
+++ code/MD_main.c
@@ -1,6 +1,6 @@
 /*
  MD_main.c
- 
+
  Created by Anders Lindman on 2013-10-31.
  */
 
@@ -10,29 +10,109 @@
 #include <time.h>
 #include "initfcc.h"
 #include "alpotential.h"
+#define nbr_of_particles 256
+#define nbr_of_timesteps 1000
+#define nbr_of_dimensions 3
 
 
 /* Main program */
 int main()
 {
-    
+    srand(time(NULL));
+
+    /* Simulation parameters */
+
+    double lattice_spacing;
+    double initial_displacement;
+
+    int n_unit_cell;
+    double lattice_param;
+
+    double timestep;
+
+    FILE *file1;
+
+    /* Current displacement, velocities, and acceleratons */
+    double q[nbr_of_particles][nbr_of_dimensions];
+
+    /* Allocate memory for large vectors */
+    /* Create 3 dimensional data by placing iniitalizeing a 1-dimensional array*/
+    #define qq(i,j,k) (disp_arr[nbr_of_particles*nbr_of_dimensions*i+nbr_of_dimensions*j+k])
+    double* disp_arr =(double *)malloc(nbr_of_timesteps*nbr_of_particles*nbr_of_dimensions*sizeof(double));
+
+    /* Initialize parameters*/
+    initial_displacement = 0.05;
+    timestep = 0.1;
+
+    /* Initialize arrays */
+    for (int i = 0; i < nbr_of_particles; i++){
+        for (int j = 0; j < nbr_of_dimensions; j++){
+            q[i][j] = 0;
+        }
+    }
+    for (int i  = 0; i < nbr_of_timesteps; i++){
+        for (int j = 0; j < nbr_of_particles; j++){
+            for (int k = 0; k < nbr_of_dimensions; k++){
+                qq(i,j,k) = 0;
+            }
+        }
+    }
+
+
+    /* Initial conditions */
+    for (int i = 0; i < nbr_of_particles; i++){
+        for (int j = 0; j < nbr_of_dimensions; j++){
+            q[i][j] = ((double) rand() / (double) RAND_MAX-0.5)*initial_displacement;
+            qq(0,i,j)=q[i][j];
+        }
+    }
+
+
+    /* Simulation */
+    for (int i = 1; i < nbr_of_timesteps; i++)
+    {
+        /* Save current displacements to array*/
+        for (int j = 0; j < nbr_of_particles; j++){
+            for (int k = 0; k < nbr_of_dimensions; k++){
+                qq(i,j,k)=q[j][k];
+            }
+        }
+    }
+
+
+    /* Save data to file*/
+    file1 = fopen("displacement.dat","w");
+
+    double current_time;
+    for (int i = 0; i < nbr_of_timesteps; i ++)
+    {
+        current_time = i*timestep;
+        fprintf(file1, "%.4f \t", current_time );
+        for (int j = 0; j < nbr_of_particles; j++)
+        {
+            for (int k = 0; k < nbr_of_dimensions; k++)
+            {
+                fprintf(file1, "%.4f \t", qq(i,j,k));
+            }
+        }
+        fprintf(file1, "\n");
+    }
+    fclose(file1);
+
+    free(array);
+
     /*
-     Code for generating a uniform random number between 0 and 1. srand should only
-     be called once.
-    */
-    /*
-     srand(time(NULL));
      double random_value;
      random_value = (double) rand() / (double) RAND_MAX;
     */
-    
+
     /*
      Descriptions of the different functions in the files initfcc.c and
      alpotential.c are listed below.
     */
-    
-    /* 
-     Function that generates a fcc lattice in units of [Å]. Nc is the number of 
+
+    /*
+     Function that generates a fcc lattice in units of [Å]. Nc is the number of
      primitive cells in each direction and a0 is the lattice parameter. The
      positions of all the atoms are stored in pos which should be a matrix of the
      size N x 3, where N is the number of atoms. The first, second and third column
@@ -41,38 +121,38 @@ int main()
     /*
      init_fcc(pos, Nc, a0);
     */
-    
-    /* 
+
+    /*
      Function that calculates the potential energy in units of [eV]. pos should be
-     a matrix containing the positions of all the atoms, L is the length of the 
+     a matrix containing the positions of all the atoms, L is the length of the
      supercell and N is the number of atoms.
     */
     /*
      double energy;
      energy = get_energy_AL(pos, L, N);
     */
-    
-    /* 
+
+    /*
      Function that calculates the virial in units of [eV]. pos should be a matrix
-     containing the positions of all the atoms, L is the length of the supercell 
+     containing the positions of all the atoms, L is the length of the supercell
      and N is the number of atoms.
     */
     /*
      double virial;
      virial = get_virial_AL(pos, L, N);
     */
-    
+
     /*
-     Function that calculates the forces on all atoms in units of [eV/Å]. the 
+     Function that calculates the forces on all atoms in units of [eV/Å]. the
      forces are stored in f which should be a matrix of size N x 3, where N is the
      number of atoms and column 1,2 and 3 correspond to the x,y and z component of
-     the force resepctively . pos should be a matrix containing the positions of 
+     the force resepctively . pos should be a matrix containing the positions of
      all the atoms, L is the length of the supercell and N is the number of atoms.
     */
     /*
      get_forces_AL(f,pos, L, N);
     */
-    
-    
-    
+
+
+
 }
