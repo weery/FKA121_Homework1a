d04c0fd3eef48deb0b1438c757cfa9bf7eca50cd
Simon Nilsson <simnilss@student.chalmers.se>
2016-11-23 18:31:09 +0100

    Worked on the report in general, added an entry to the references and some minor cleanup in the Task1 code.

--- code/Task1/MD_main.c
+++ code/Task1/MD_main.c
@@ -35,23 +35,26 @@ int main()
 
     int time_length = 10;
 
-    /* Initialize parameters*/
+
+
+    /* Initialize parameters */
+    /* ===================== */
+
     initial_displacement = 0.05;
     lattice_param = 4.046; // For aluminium (Å)
     lattice_spacing = lattice_param/sqrt(2.0);
-    //timestep = 0.01; // 0.1 Bad, 0.01 Seems decent
 
     m_AL = 0.0027964; // In ASU
     cell_length = 4*lattice_param;  // Side of the supercell: The 256 atoms are
                                     // structured in a block of 4x4x4 unit cells
 
-    // Test different timestep with 0.01 Å difference
+    /* Test different timestep with 0.01 Å difference */
     for (int i = 0; i < 8; i++)
-        timesteps[i]=0.005*(i+1);
+        timesteps[i] = 0.005 * (i+1);
 
-    for (int t = 0; t < 8; t++)
-    {
-        // Current timestep and number of timesteps
+    for (int t = 0; t < 8; t++) {
+
+        /* Current timestep and number of timesteps */
         double timestep = timesteps[t];
         int nbr_of_timesteps = (int)(time_length/timestep);
 
@@ -63,14 +66,16 @@ int main()
 
 
         /* Allocate memory for large vectors */
-        /* Simulate 3 dimensional data by placing iniitalizeing a 1-dimensional array*/
         double* energy_pot =(double*)malloc(nbr_of_timesteps*sizeof(double));
         double* energy_kin = (double*)malloc(nbr_of_timesteps*sizeof(double));
 
         /* Put atoms on lattice */
         init_fcc(q, 4, lattice_param);
 
+
+
         /* Initial conditions */
+        /* ================== */
 
         for (int i = 0; i < nbr_of_particles; i++){
             for (int j = 0; j < nbr_of_dimensions; j++){
@@ -86,7 +91,10 @@ int main()
         get_forces_AL(f,q,cell_length,nbr_of_particles);
 
 
+
         /* Simulation */
+        /* ========== */
+
         for (int i = 1; i < nbr_of_timesteps; i++)
         {
             /** Verlet algorithm **/
@@ -97,7 +105,7 @@ int main()
                 }
             }
 
-            /* Update displacement*/
+            /* Update displacement */
             for (int j = 0; j < nbr_of_particles; j++){
                 for (int k = 0; k < nbr_of_dimensions; k++){
                     q[j][k] += timestep * v[j][k];
@@ -107,26 +115,29 @@ int main()
             /* Forces */
             get_forces_AL(f,q,cell_length,nbr_of_particles);
 
-            /* Final velocity*/
+            /* Final velocity */
             for (int j = 0; j < nbr_of_particles; j++){
                 for (int k = 0; k < nbr_of_dimensions; k++){
                     v[j][k] += timestep * 0.5 * f[j][k]/m_AL;
                 }
             }
 
+
             /* Calculate energy */
-            // Potential energy
+            /* ================ */
+
+            /* Potential energy */
             energy_pot[i] = get_energy_AL(q,cell_length,nbr_of_particles);
-            // Kinetic energy
+            /* Kinetic energy */
             energy_kin[i] = get_kinetic_AL(v,nbr_of_dimensions,nbr_of_particles,m_AL);
         }
         char str[80];
         char S[3];
         sprintf(S, "%.3f", timestep);
 
-          strcpy (str,"data/energy");
-          strcat (str,S);
-          strcat (str,".dat");
+        strcpy (str,"data/energy");
+        strcat (str,S);
+        strcat (str,".dat");
 
 
         /* Save energies to file */
